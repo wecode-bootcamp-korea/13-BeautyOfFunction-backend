863012cd08cfc87931dba106775c13c441470954
sojeong20 <sojeong.heo@gmail.com>
2020-11-07 15:47:03 +0900

    Modify: User 모델 수정 - social login id field 추가 - password에 null=True 속성 추가

--- user/models.py
+++ user/models.py
@@ -2,19 +2,20 @@ from django.db          import models
 from creditcards.models import CardNumberField, CardExpiryField
 
 class User(models.Model):
-    email          = models.EmailField(max_length=254, unique=True)
-    password       = models.CharField(max_length=1000)
-    first_name     = models.CharField(max_length=45, null=True)
-    last_name      = models.CharField(max_length=45, null=True)
-    address1       = models.CharField(max_length=45, null=True)
-    address2       = models.CharField(max_length=45, null=True)
-    country        = models.CharField(max_length=45, null=True)
-    city           = models.CharField(max_length=45, null=True)
-    state_province = models.CharField(max_length=45, null=True)
-    zip_code       = models.IntegerField(null=True)
-    phone_number   = models.CharField(max_length=20, null=True)
-    created_at     = models.DateField(auto_now_add=True)
-    updated_at     = models.DateField(null=True)
+    email           = models.EmailField(max_length=254, unique=True)
+    password        = models.CharField(max_length=1000, null=True)
+    first_name      = models.CharField(max_length=45, null=True)
+    last_name       = models.CharField(max_length=45, null=True)
+    address1        = models.CharField(max_length=45, null=True)
+    address2        = models.CharField(max_length=45, null=True)
+    country         = models.CharField(max_length=45, null=True)
+    city            = models.CharField(max_length=45, null=True)
+    state_province  = models.CharField(max_length=45, null=True)
+    zip_code        = models.IntegerField(null=True)
+    phone_number    = models.CharField(max_length=20, null=True)
+    created_at      = models.DateField(auto_now_add=True)
+    updated_at      = models.DateField(null=True)
+    social_login_id = models.CharField(max_length=45, null=True)
 
     class Meta:
         db_table = 'users'
