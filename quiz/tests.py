38e459f00a8ae2403eb94fa742d994c4178f9c2f
sojeong20 <sojeong.heo@gmail.com>
2020-11-12 23:15:51 +0900

    Add: 퀴즈 엔드포인트 구현

--- quiz/tests.py
+++ quiz/tests.py
@@ -1,3 +1,369 @@
-from django.test import TestCase
+from django.test      import TestCase, Client
 
-# Create your tests here.
+from quiz.models import (
+    HairType,
+    HairStructure,
+    ScalpMoisture,
+    HairGoal,
+    Fragrance,
+    FragranceStrength,
+)
+from product.models import (
+    Color,
+    ProductTemplate,
+    CategoryColor,
+    Category
+)
+from user.models import Frequency
+
+client = Client()
+
+class HairProfileTest(TestCase):
+    def setUp(self):
+        HairType.objects.create(id=1, hair_type="test1")
+        HairStructure.objects.create(id=2, hair_structure="test2")
+        ScalpMoisture.objects.create(id=3, scalp_moisture="test3")
+
+    def tearDown(self):
+        HairType.objects.get(id=1).delete()
+        HairStructure.objects.get(id=2).delete()
+        ScalpMoisture.objects.get(id=3).delete()
+
+    def test_hair_profile_success(self):
+        hair_profile = [{
+            'hair_types': [{
+                'id'  : 1,
+                'type': 'test1'
+            }],
+            'hair_structures': [{
+                'id'       : 2,
+                'structure': 'test2'
+            }],
+            'scalp_moistures': [{
+                'id'      : 3,
+                'moisture': 'test3'
+            }]
+        }]
+
+        response = client.get('/quiz/hair-profile')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), {"hair_profile": hair_profile})
+
+    def test_hair_profile_fail(self):
+        response = client.get('/quiz/hairprofile')
+        self.assertEqual(response.status_code, 404)
+
+class HairGoalTest(TestCase):
+    def setUp(self):
+        functions = [
+            HairGoal(
+                id=i, function=f"test{i}"
+            )
+            for i in range(1,4)
+        ]
+        HairGoal.objects.bulk_create(objs=functions)
+
+    def tearDown(self):
+        HairGoal.objects.filter(id__lte=3).delete()
+
+    def test_hair_goal_success(self):
+        hair_goals = [
+            {
+                'id': 1,
+                'goal': 'test1'
+            },
+            {
+                'id':2,
+                'goal': 'test2'
+            },
+            {
+                'id': 3,
+                'goal': 'test3'
+            }
+        ]
+
+        response = client.get('/quiz/hair-goals')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), {"hair_goals": hair_goals})
+
+    def test_hair_goal_fail(self):
+        response = client.get('/quiz/hair-goal')
+        self.assertEqual(response.status_code, 404)
+
+class CustomizingTest(TestCase):
+    def setUp(self):
+        fragrances = [
+            Fragrance(
+                id=i, fragrance=f"scent{i}", fragrance_image=f"url{i}"
+            )
+            for i in range(1,5)
+        ]
+        Fragrance.objects.bulk_create(objs=fragrances)
+
+        FragranceStrength.objects.create(id=1, strength="light")
+        FragranceStrength.objects.create(id=2, strength="strong")
+
+        colors = [
+            Color(
+                id=i, color_code=f"{i}"
+            )
+            for i in range(1,10)
+        ]
+        Color.objects.bulk_create(objs=colors)
+        Category.objects.create(id=1,name="test",description="test",ingredients_image_url="test",recommended_image_url="test",great_for_image_url="test",ingredients="test",recommended="test",great_for="test",short_description="test")
+        images = [
+            CategoryColor(
+                category_id=1, color_id=i, product_image_url=f"url{i}"
+            )
+            for i in range(1,10)
+        ]
+        CategoryColor.objects.bulk_create(objs=images)
+
+    def tearDown(self):
+        Color.objects.filter(id__lte=9).delete()
+        Fragrance.objects.filter(id__lte=4).delete()
+        FragranceStrength.objects.filter(id__lte=2).delete()
+        Category.objects.get(id=1).delete()
+        CategoryColor.objects.filter(id__lte=9).delete()
+
+    def test_customizing_purple_shampoo_success(self):
+        fragrance_options = [
+            {
+                'fragrances': [
+                    {
+                        'id': 1,
+                        'fragrance': 'scent1',
+                        'image': 'url1'
+                    },
+                    {
+                        'id': 2,
+                        'fragrance': 'scent2',
+                        'image': 'url2'
+                    },
+                    {
+                        'id': 3,
+                        'fragrance': 'scent3',
+                        'image': 'url3'
+                    },
+                    {
+                        'id': 4,
+                        'fragrance': 'scent4',
+                        'image': 'url4'
+                    }
+                ],
+                'strengths': [
+                    {
+                        'id': 1,
+                        'strength': 'light'
+                    },
+                    {
+                        'id': 2,
+                        'strength': 'strong'
+                    }
+                ]
+            }
+        ]
+        shampoo_colors = [
+            {
+                'id': 9,
+                'color_code': '9',
+                'color_image': 'url9'
+            }
+        ]
+
+        conditioner_colors = [
+            {
+                'id': 1,
+                'color_code': '1',
+                'color_image': 'url1'
+            },
+            {
+                'id': 2,
+                'color_code': '2',
+                'color_image': 'url2'
+            },
+            {
+                'id': 3,
+                'color_code': '3',
+                'color_image': 'url3'
+            },
+            {
+                'id': 4,
+                'color_code': '4',
+                'color_image': 'url4'
+            },
+            {
+                'id': 5,
+                'color_code': '5',
+                'color_image': 'url5'
+            },
+
+            {
+                'id': 6,
+                'color_code': '6',
+                'color_image': 'url6'
+            },
+            {
+                'id': 7,
+                'color_code': '7',
+                'color_image': 'url7'
+            },
+
+            {
+                'id': 8,
+                'color_code': '8',
+                'color_image': 'url8'
+            },
+        ]
+ 
+        response = client.post('/quiz/appearance-and-fragrance', {"purple_shampoo": True}, content_type='application/json')
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), {
+            "fragrance_options" : fragrance_options,
+            "shampoo_colors"    : shampoo_colors,
+            "conditioner_colors": conditioner_colors
+        })
+
+    def test_customizing_normal_shampoo_success(self):
+        fragrance_options = [
+            {
+                'fragrances': [
+                    {
+                        'id': 1,
+                        'fragrance': 'scent1',
+                        'image': 'url1'
+                    },
+                    {
+                        'id': 2,
+                        'fragrance': 'scent2',
+                        'image': 'url2'
+                    },
+                    {
+                        'id': 3,
+                        'fragrance': 'scent3',
+                        'image': 'url3'
+                    },
+                    {
+                        'id': 4,
+                        'fragrance': 'scent4',
+                        'image': 'url4'
+                    }
+                ],
+                'strengths': [
+                    {
+                        'id': 1,
+                        'strength': 'light'
+                    },
+                    {
+                        'id': 2,
+                        'strength': 'strong'
+                    }
+                ]
+            }
+        ]
+        shampoo_colors = [
+            {
+                'id': 1,
+                'color_code': '1',
+                'color_image': 'url1'
+            },
+            {
+                'id': 2,
+                'color_code': '2',
+                'color_image': 'url2'
+            },
+            {
+                'id': 3,
+                'color_code': '3',
+                'color_image': 'url3'
+            },
+            {
+                'id': 4,
+                'color_code': '4',
+                'color_image': 'url4'
+            },
+            {
+                'id': 5,
+                'color_code': '5',
+                'color_image': 'url5'
+            },
+
+            {
+                'id': 6,
+                'color_code': '6',
+                'color_image': 'url6'
+            },
+            {
+                'id': 7,
+                'color_code': '7',
+                'color_image': 'url7'
+            },
+
+            {
+                'id': 8,
+                'color_code': '8',
+                'color_image': 'url8'
+            },
+        ]
+        conditioner_colors = [
+            {
+                'id': 1,
+                'color_code': '1',
+                'color_image': 'url1'
+            },
+            {
+                'id': 2,
+                'color_code': '2',
+                'color_image': 'url2'
+            },
+            {
+                'id': 3,
+                'color_code': '3',
+                'color_image': 'url3'
+            },
+            {
+                'id': 4,
+                'color_code': '4',
+                'color_image': 'url4'
+            },
+            {
+                'id': 5,
+                'color_code': '5',
+                'color_image': 'url5'
+            },
+
+            {
+                'id': 6,
+                'color_code': '6',
+                'color_image': 'url6'
+            },
+            {
+                'id': 7,
+                'color_code': '7',
+                'color_image': 'url7'
+            },
+
+            {
+                'id': 8,
+                'color_code': '8',
+                'color_image': 'url8'
+            },
+        ]
+        response = client.post('/quiz/appearance-and-fragrance', {"purple_shampoo": False}, content_type='application/json')
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), {
+            "fragrance_options" : fragrance_options,
+            "shampoo_colors"    : shampoo_colors,
+            "conditioner_colors": conditioner_colors
+        })
+
+#    def test_customizing_key_error(self):
+#        response = client.post('/quiz/appearance-and-fragrance', content_type='application/json')
+#        self.assertEqual(response.status_code, 400)
+
+#    def test_customizint_fail(self):
+#        response = Client.post('/quiz/fragrance-and-appearance', content_type='application/json')
+#        self.assertEqual(response.status_code, 404)
+
--- quiz/views.py
+++ quiz/views.py
@@ -1,3 +1,145 @@
-from django.shortcuts import render
+import json
+import requests
 
-# Create your views here.
+from django.http  import JsonResponse
+from django.views import View
+
+from quiz.models import (
+    HairType,
+    HairStructure,
+    ScalpMoisture,
+    HairGoal,
+    Fragrance,
+    FragranceStrength
+)
+from product.models import (
+    Product,
+    ProductTemplate,
+    Color,
+    CategoryColor,
+    DetailImage
+)
+from user.models import Subscription, Frequency
+from utils       import query_debugger
+
+class HairProfileView(View):
+    def get(self, request):
+        hair_types = HairType.objects.all()
+        structures = HairStructure.objects.all()
+        moistures  = ScalpMoisture.objects.all()
+
+        hair_profile = [{
+            'hair_types': [{
+                'id'  : hair_type.id,
+                'type': hair_type.hair_type
+            } for hair_type in hair_types],
+            'hair_structures': [{
+                'id'       : structure.id,
+                'structure': structure.hair_structure
+            } for structure in structures],
+            'scalp_moistures': [{
+                'id'      : moisture.id,
+                'moisture': moisture.scalp_moisture
+            } for moisture in moistures]
+        }]
+
+        return JsonResponse({"hair_profile": hair_profile}, status=200)
+
+class HairGoalView(View):
+    def get(self, request):
+        goals = HairGoal.objects.all()
+
+        hair_goals = [{
+            'id'  : goal.id,
+            'goal': goal.function
+        } for goal in goals]
+
+        return JsonResponse({"hair_goals": hair_goals}, status=200)
+
+class CustomizingView(View):
+    @query_debugger
+    def post(self, request):
+        try:
+            data = json.loads(request.body)
+            purple_shampoo = data['purple_shampoo']
+
+            fragrances = Fragrance.objects.all()
+            strengths  = FragranceStrength.objects.all()
+
+            fragrance_options = [{
+                'fragrances': [{
+                    'id'       : fragrance.id,
+                    'fragrance': fragrance.fragrance,
+                    'image'    : fragrance.fragrance_image
+                } for fragrance in fragrances],
+                'strengths': [{
+                    'id'      : strength.id,
+                    'strength': strength.strength
+                } for strength in strengths]
+            }]
+
+            shampoo_colors     = Color.objects.prefetch_related('categorycolor_set')[:8] if purple_shampoo == False else Color.objects.filter(id=9)
+            conditioner_colors = Color.objects.prefetch_related('categorycolor_set')[:8]
+
+            shampoo_colors = [{
+                'id'         : color.id,
+                'color_code' : color.color_code,
+                'color_image': color.categorycolor_set.all()[0].product_image_url
+            } for color in shampoo_colors]
+
+            conditioner_colors = [{
+                'id'         : color.id,
+                'color_code' : color.color_code,
+                'color_image': color.categorycolor_set.all()[0].product_image_url
+            } for color in conditioner_colors]
+
+            return JsonResponse({
+                "fragrance_options" : fragrance_options,
+                "shampoo_colors"    : shampoo_colors,
+                "conditioner_colors": conditioner_colors
+            }, status=200)
+
+        except KeyError:
+            return JsonResponse({"Message": "KEY_ERROR"}, status=400)
+
+class ProductOptionView(View):
+    def post(self, request):
+        try:
+            data = json.loads(request.body)
+
+            shampoo_color_id     = data['shampoo_color']
+            conditioner_color_id = data['conditioner_color']
+
+            frequencies = Frequency.objects.all()
+
+            frequency_options = [{
+                'id': frequency.id,
+                'option': frequency.option
+            } for frequency in frequencies]
+
+            product_templates = ProductTemplate.objects.all()
+            shampoo_image     = CategoryColor.objects.get(color_id=shampoo_color_id).product_image_url
+            conditioner_image = CategoryColor.objects.get(color_id=conditioner_color_id).product_image_url
+
+            size_options = [{
+                'shampoo_image'    : shampoo_image,
+                'conditioner_image': conditioner_image,
+                'size_options': [{
+                    'id'   : template.id,
+                    'name' : template.name,
+                    'price': template.price
+                } for template in product_templates]
+            }]
+
+            return JsonResponse({
+                "size_options"     : size_options,
+                "frequency_options": frequency_options
+            }, status =200)
+
+        except KeyError:
+            return JsonResponse({"Message": "KEY_ERROR"}, status=400)
+
+
+
+
+
